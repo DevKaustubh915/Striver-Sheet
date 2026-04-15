22512ba39bdc13ce9f3321aa1a794be2a4fb71c3
DevKaustubh <kaustubh915b@gmail.com>
2026-04-15 17:53:35 +0530

    question2

---
+++ pattern based questions/pattern2.java
@@ -0,0 +1,28 @@
+
+/*
+
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+
+*/
+
+
+public class pattern2 {
+
+    public static void printPattern() {
+        for (int i = 0 ; i < 5; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printPattern();
+    }
+}
